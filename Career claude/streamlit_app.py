dde25d0fa71e56e6c83f986d391ac2182829f674
Ishmal00 <iznasili00@gmail.com>
2026-03-01 02:33:20 +0500

    Update streamlit_app.py

--- Career claude/streamlit_app.py
+++ Career claude/streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit.components.v1 as components
 st.title("My Career Counselor App")
 
 # Apni HTML file ko read karke yahan display karein
-with open("Career claude/career-counselor.html", "r") as f:
+with open("career-counselor.html", "r") as f:
     html_code = f.read()
     components.html(html_code, height=800, scrolling=True)
